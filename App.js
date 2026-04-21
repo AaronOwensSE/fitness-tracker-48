781efdc2c185d764a818882fbb33b0d5e9fa6159
AaronOwensSE <aaron.owens.se@gmail.com>
2026-04-20 21:29:08 -0500

    Add database setup at app start.

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 // External Dependencies
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 // Internal Dependencies
 import LandingPage from "./pages/LandingPage.js";
@@ -7,9 +7,28 @@ import InputsPage from "./pages/InputsPage.js";
 import OutputsPage from "./pages/OutputsPage.js";
 import PRsPage from "./pages/PRsPage.js";
 import AddPRPage from "./pages/AddPRPage.js";
+import database from "./services/database.js";
+import ErrorMessagePage from "./pages/ErrorMessagePage.js";
 
 export default function App() {
     const [ navPage, setNavPage ] = useState("LandingPage");
+    const [ errorMessage, setErrorMessage ] = useState(null);
+
+    useEffect( () => {
+        const load = async () => {
+            try {
+                await database.buildSchema();
+            } catch (error) {
+                setErrorMessage("Database setup error.");
+            }
+        };
+
+        load();
+    }, [] );
+
+    if (errorMessage !== null) {
+        return <ErrorMessagePage errorMessage={errorMessage} onNavigate={setNavPage} />;
+    }
 
     switch (navPage) {
         case "LandingPage":
